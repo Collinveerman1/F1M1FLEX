818787547f45a7f111098d85a2abfa74e97da47c
Collinveerman1 <collinveerman1@gmail.com>
2021-11-05 14:57:54 +0100

    tweede commit

--- 04-DiscordBot/discord-bot.py
+++ 04-DiscordBot/discord-bot.py
@@ -3,13 +3,20 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    # De bot kan op veel verschillende servers draaien. Met deze regel code pak je de eerste server die deze bot heeft. Als jouw bot maar 1 server heeft is het makkelijk.
     guild = client.guilds[0]
-    # De naam van de server printen we gelijk uit.
+    channel = guild.text_channels[0]
+    print(channel.name, "is the name of the channel")
+    await channel.send("I'm online!")
+    print(client.user, "has connected to discord")
     print(guild.name, "is the name of the server")
 
-    # We printen de naam van de bot user uit.
-    print(client.user, "has connected to the client")
-
-
-client.run("OTAzNjI0Njg0NDY0MTExNjI2.YXvsEQ.IE0EjEuA9NuFLIWocijVypRVYpw")
+@client.event
+async def on_message(message):
+    print(message.channel.name, "the message was posted from this channel")
+    print(message.content)
+    print(message.author,"is the user who wrote the message")
+    print(message.created_at,"is when the message was posted")
+    print(message.channel,"is the channel this message was posted in")
+    if message.author.bot == False:
+        await message.channel.send("Hello " + str(message.author))
+client.run("OTAzNjI0Njg0NDY0MTExNjI2.YXvsEQ.znIeb9JwTJCpjtBjEQSWctaoBng")
